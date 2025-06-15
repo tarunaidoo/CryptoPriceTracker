1e927dfbfd2d8629ab4ae52c85c0858ca4e90552
tarunaidoo <taruna.naidoo05@gmail.com>
2025-06-15 19:12:17 +0200

    Navbar component set up with buttons and select menu. CSS applied to navbar.

--- cryptoTracker/src/App.jsx
+++ cryptoTracker/src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-
+import React from 'react';
+import Navbar from './components/Navbar/Navbar.jsx';
 function App() {
 
   return (
-    <>
-    </>
+    <div className="app">
+      <Navbar/>
+    </div>
   )
 }
 
